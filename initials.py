4efc9363470cb06e5a7ecaf5d2bd6e07b9b7195d
Blessed Kelvin Owusu Asemanyi <blesseddd.96@gmail.com>
2026-05-31 20:47:47 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
+# File: initials.py
+# Description: Print out my initials
+# Assignment Number: 1
+
+# Name: Blessed Kelvin Asemanyi
+# STUDENT ID: 2425400417
+# Email: blesseddd.96@gmail.com
+# Grader: Augustus Buckman
+
+# On my honor, Blessed Kelvin Asemanyi, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
-
+    # This function coordinates the printing of the small initials and the large block initials.
+    print()
+    print("...BKA")
+    print()
+    print("...BBBBBBBBB..........KK........KK.............AA........")
+    print("...BB.......BB........KK.......KK.............AAAA.......")
+    print("...BB........BB.......KK.....KK..............AA..AA......")
+    print("...BB.......BB........KK...KK...............AA....AA.....")
+    print("...BBBBBBBBBB.........KK.KK................AA.....AA.....")
+    print("...BB.......BB........KKKK.................AAAAAAAAAA....")
+    print("...BB........BB.......KK..KK..............AA........AA...")
+    print("...BB........BB.......KK....KK............AA........AA...")
+    print("...BB.......BB...**...KK......KK.....**...AA........AA...")
+    print("...BBBBBBBBB.....**...KK........KK...**...AA........AA...")
+    print()
 
 
 main()
